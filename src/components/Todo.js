117c982617deb374558c501a26ea084a9f8b7368
egesua <egesu06@gmail.com>
2024-03-12 20:52:56 +0300

    search action and button ,flexbox created.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react"
-import { BsPlus } from "react-icons/bs";
+import { BsPlus, BsSearch } from "react-icons/bs";
 import { useDispatch } from "react-redux";
-import { addTodo } from "../redux/actions";
+import { addTodo, updateSearchTerm } from "../redux/actions";
 
 const Todo = () => {
 
     const dispatch = useDispatch()
 
     const [newTodoText, setNewTodoText] = useState("");
+    const [searchTerm , setSearchTerm] = useState("")
 
     const handleAddTodo = (text) => {
         dispatch(addTodo);
@@ -20,10 +21,15 @@ const Todo = () => {
         }
     }
 
+    const handleSearchChange = (value) => {
+        setSearchTerm(value);
+        dispatch(updateSearchTerm(value));
+    }
+
   return (
     <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
       <h2 className="mt-3 mb-6 text-2xl font-bold text-center uppercase">PERSONAL TODO APP</h2>
-
+        {/* input text and button */}
       <div className="flex items-center mb-4">
             <input value={newTodoText} onChange={(e) => setNewTodoText(e.target.value)} type="text" name="text" id="addTodoInput" placeholder="Add Todo"
             className=" flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"/>
@@ -31,7 +37,17 @@ const Todo = () => {
             className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none" onClick={handleAddTodoClick}><BsPlus /></button>
       </div>
 
-      
+        {/* filter and search */}
+        <div>
+            <div className="flex items-center mb-4">
+            <input value={searchTerm} onChange={(e) => handleSearchChange(e.target.value)} type="text" name="text" id="addTodoInput" placeholder="Search"
+            className=" flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"/>
+            <button 
+            className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none" onClick={handleAddTodoClick}><BsSearch /></button>
+            </div>
+            
+        </div>
+
     </div>
   )
 };

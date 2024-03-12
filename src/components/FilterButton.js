b100fbccaa1283afe80c8b03fb29f02a31f0ac65
egesua <egesu06@gmail.com>
2024-03-12 21:19:45 +0300

    filter button kind of completed.

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,14 +1,27 @@
 import React from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { filterTodo, markAllCompleted } from "../redux/actions";
 
 const FilterButton= () => {
+
+    const dispatch = useDispatch();
+    const currentFilter = useSelector((state) => state.filter);
+    const handleFilter = (filter) => {
+        dispatch(filterTodo(filter))
+    }
+
+
   return (
     <div className="flex space-x-4 items-center">
-      <select className="text-sm px-2 py-1 rounded border border-gray-200 focus:outline-none">
+      <select 
+      value={currentFilter}
+      onChange={(e) => handleFilter(e.target.value)} 
+      className="text-sm px-2 py-1 rounded border border-gray-200 focus:outline-none">
         <option value="ALL">Default</option>
         <option value="COMPLETED">Completed</option>
         <option value="INCOMPLETE">Incomplete</option>
       </select>
-      <button className="text-sm px-2 py-1 bg-purple-500 text-white ml-2 rounded">Mark All Completed</button>
+      <button onClick={() => dispatch(markAllCompleted())} className="text-sm px-2 py-1 bg-purple-500 text-white ml-2 rounded">Mark All Completed</button>
     </div>
   )
 };

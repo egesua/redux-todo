f139c138b4a6c028408bbf48f7ea16d671fda8b3
egesua <egesu06@gmail.com>
2024-03-12 22:09:16 +0300

    todo item mapping fixed. Also conditional rendering training.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,34 @@
-import React from "react"
+import { useSelector } from "react-redux";
+import TodoItem from "./TodoItem";
 
-const TodoList = (props) => {
+const TodoList = () => {
+  const filteredTodos = useSelector((state) => {
+    const todos = state.todos;
+    const filter = state.filter;
+    const searchTerm = state.searchTerm;
+
+    return todos.filter((todo) => {
+      const matchesFilter =
+        (filter === "COMPLETED" && todo.completed) ||
+        (filter === "INCOMPLETED" && !todo.completed) ||
+        filter === "ALL";
+
+      const matchesSeacrh = todo.text.toLowerCase().includes(searchTerm);
+
+      return matchesFilter && matchesSeacrh;
+    });
+  });
+
+  console.log("Filtered Todos: " + filteredTodos);
+
   return (
-    <div>
-      Bum
-    </div>
-  )
+    <ul>
+      <li className="my-2 text-sm italic">All Your Notes Here...</li>
+      {filteredTodos.map((todo, index) => (
+        <TodoItem key={index} todo={todo} index={index}/>
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
